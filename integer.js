4d27e04b116844189592f23b1dce9547099a14a1
laziza@mail.ru <laziza00>
2022-03-04 09:37:58 +0500

    initialc ommit

--- integer.js
+++ integer.js
@@ -65,3 +65,15 @@
 // else {
 //    console.log("no");
 // }
+
+
+let a = +prompt();
+let n1 = 0;
+let n2 = 1;
+let sum;
+for (let i = 1; i <= a; i++) {
+    console.log(n1);
+    sum = n1 + n2;
+    n1 = n2;
+    n2 = sum;
+}
