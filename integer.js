5d0e5f3372eccbb64a1f4cd883805107414b0e50
laziza@mail.ru <laziza00>
2022-03-04 09:06:23 +0500

    initial commit

---
+++ integer.js
@@ -0,0 +1,67 @@
+
+// =========================problem 6
+
+// let a = +prompt("son kiriting: ")
+// let b = +prompt("son kiriting: ")
+// let c = +prompt("son kiriting: ")
+
+// if (a>b && a>c) {
+//    console.log("katta", a);
+// }
+// if (b>a && b>c) {
+//    console.log("katta", b);
+// }
+// if (c>a && c>b) {
+//    console.log("katta", c);
+// }
+
+
+
+// =========================problem 7
+
+// let a = +prompt("son kiriting: ")
+// let b = +prompt("son kiriting: ")
+// let c = +prompt("son kiriting: ")
+
+// if (a+b>c || a+c> b || c+b > a){
+//    console.log("yes");
+// }
+// else {
+//    console.log("no");
+// }
+
+// =========================problem 8
+
+// let a = +prompt("son kiriting: ")
+// let b = +prompt("son kiriting: ")
+// let c = +prompt("son kiriting: ")
+
+// if (a==b && a==c && b==c){
+//    console.log("teng tomonli uchburchak");
+// }
+// else if (a==c){
+//    console.log("teng yonli uchburchak");
+// }
+// else if (a!=b && a!=c && b!=c){
+//    console.log("turli tomonli uchburchak");
+// }
+
+
+// =========================problem 9
+
+// let a = +prompt("son kiriting: ")
+// let b = +prompt("son kiriting: ")
+// let c = +prompt("son kiriting: ")
+
+// if (a*a + b*b == c*c){
+//    console.log("yes");
+// }
+// else if (a*a + c*c == b*b){
+//    console.log("yes");
+// }
+// else if (b*b + c*c == a*a){
+//    console.log("yes");
+// }
+// else {
+//    console.log("no");
+// }
